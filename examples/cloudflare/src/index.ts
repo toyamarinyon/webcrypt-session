f74c66e2bea1dba13223acc14abfbf8e68537d0f
toyamarinyon <toyamarinyon@gmail.com>
2022-06-14 18:31:25 +0900

    add example for cloudflare

---
+++ examples/cloudflare/src/index.ts
@@ -0,0 +1,37 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `wrangler dev src/index.ts` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+import { z } from "zod";
+import { createWebCryptSession } from "../../../dist/webCryptSession";
+export interface Env {
+  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+  // MY_KV_NAMESPACE: KVNamespace;
+  //
+  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+  // MY_DURABLE_OBJECT: DurableObjectNamespace;
+  //
+  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+  // MY_BUCKET: R2Bucket;
+}
+
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const sessionScheme = z.object({
+      userId: z.number(),
+    });
+
+    const webCryptSession = createWebCryptSession(sessionScheme, request);
+    webCryptSession.session.userId = 1;
+    return webCryptSession.response("Hello World!");
+  },
+};
--- script/build.js
+++ script/build.js
@@ -1,10 +1,15 @@
-require("esbuild").build({
-  entryPoints: ["./src/webCryptSession.ts"],
+const { build } = require("esbuild");
+
+const shared = {
+  entryPoints: ["src/webCryptSession.ts"],
   bundle: true,
-  outdir: "dist",
   define: {
     "import.meta.vitest": undefined,
   },
-  minify: true,
-  sourcemap: true,
+};
+
+build({
+  ...shared,
+  format: "esm",
+  outfile: "dist/webCryptSession.js",
 });
