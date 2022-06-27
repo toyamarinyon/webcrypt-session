26d6cc1415f53e9305efde4bebfbd6bb1e9a9420
toyamarinyon <toyamarinyon@gmail.com>
2022-06-28 01:52:25 +0900

    update

--- packages/webcrypt-session/src/adapters/trpc/index.test.ts
+++ packages/webcrypt-session/src/adapters/trpc/index.test.ts
@@ -8,11 +8,19 @@ const scheme = z.object({
 });
 const defaultRequest = new Request("http://loclahost:8989/test");
 const password = "IF4B#t69!WlX$uS22blaxDvzJJ%$vEh%";
-const option: WebCryptSessionOption = { password, cookie: "session" };
+const option: WebCryptSessionOption = { password };
 
 test("works properly", async () => {
   const session = await getWebCryptSession(scheme, defaultRequest, option);
   expect(session.userId).not.toBeNull();
   // @ts-ignore we actually want to test this
-  expect(session.toHeaderValue).not.toBeNull();
+  const [key, value] = await session.responseHeader()
+  expect(key).toBe('set-cookie')
+  expect(value).toBeUndefined()
+
+  session.userId = 1
+  // @ts-ignore we actually want to test this
+  const [key2, value2] = await session.responseHeader()
+  expect(key2).toBe('set-cookie')
+  expect(value2).not.toBeUndefined()
 });
--- packages/webcrypt-session/src/adapters/trpc/index.ts
+++ packages/webcrypt-session/src/adapters/trpc/index.ts
@@ -1,5 +1,13 @@
 import { z } from "zod";
 import { createWebCryptSession, WebCryptSessionOption } from "../..";
+import {
+  AnyRouter,
+  inferRouterContext,
+  inferRouterError,
+  ProcedureType,
+  TRPCError,
+} from "@trpc/server";
+import { TRPCResponse } from "@trpc/server/dist/declarations/src/rpc";
 
 export async function getWebCryptSession<T extends z.AnyZodObject>(
   scheme: T,
@@ -8,7 +16,23 @@ export async function getWebCryptSession<T extends z.AnyZodObject>(
 ) {
   const webCryptSession = await createWebCryptSession(scheme, req, option);
 
-  // Enclose calls to WebcryptSession's internal functions
+  // Call this function inside tRPC.responseMeta to set cookie into header
+  async function responseHeader<TRouter extends AnyRouter>(opts: {
+    data: TRPCResponse<unknown, inferRouterError<TRouter>>[];
+    ctx?: inferRouterContext<TRouter>;
+    paths?: string[];
+    type: ProcedureType | "unknown";
+    errors: TRPCError[];
+  }) {
+    const setCookieHeader = await webCryptSession.toHeaderValue();
+    return ["set-cookie", setCookieHeader];
+  }
+
+  Object.assign(webCryptSession, {
+    responseHeader,
+  });
+
+  // Block calls to WebCryptSession's internal functions
   // allowing access only to the scheme.
   return (webCryptSession as unknown) as z.infer<T>;
 }
