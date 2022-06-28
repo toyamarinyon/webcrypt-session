a2be98c5c519d93961dd606034c2c64295013347
toyamarinyon <toyamarinyon@gmail.com>
2022-06-28 19:42:18 +0900

    Update api signature (#17)
    * update
    
    * update example
    
    * add test #16
    
    * add changeset

--- packages/webcrypt-session/src/index.test.ts
+++ packages/webcrypt-session/src/index.test.ts
@@ -51,7 +51,7 @@ test("session not exists", async () => {
     option
   );
   expect(webCryptSession.userId).toBeUndefined();
-  const setCookieHeader = await webCryptSession.toHeaderValue();
+  const setCookieHeader = webCryptSession.toHeaderValue();
   expect(setCookieHeader).toBeUndefined();
 });
 
@@ -68,7 +68,7 @@ test("session exists", async () => {
   expect(webCryptSession.userId).toBe(1);
 });
 
-test("update session", async () => {
+test("save session", async () => {
   const webCryptSession = await createWebCryptSession(
     scheme,
     new Request("http://loclahost:8989/test"),
@@ -77,9 +77,10 @@ test("update session", async () => {
   expect(webCryptSession.userId).toBeUndefined();
   const nonSessionHeader = await webCryptSession.toHeaderValue();
   expect(nonSessionHeader).toBeUndefined();
-
-  webCryptSession.userId = 1;
-  const sessionHeader = await webCryptSession.toHeaderValue();
+  await webCryptSession.save({
+    userId: 1,
+  });
+  const sessionHeader = webCryptSession.toHeaderValue();
   expect(sessionHeader).not.toBeUndefined();
 });
 
@@ -93,7 +94,22 @@ test("invalid session", async () => {
     }),
     option
   );
-  const nonSessionHeader = await webCryptSession.toHeaderValue();
+  const nonSessionHeader = webCryptSession.toHeaderValue();
+  expect(webCryptSession.userId).toBeUndefined();
+  expect(nonSessionHeader).toBeUndefined();
+});
+
+test("there is no session in cookie", async () => {
+  const webCryptSession = await createWebCryptSession(
+    scheme,
+    new Request("http://loclahost:8989/test", {
+      headers: {
+        cookie: "googleAnalytics=%7B%22userId%22%3A1%7",
+      },
+    }),
+    option
+  );
+  const nonSessionHeader = webCryptSession.toHeaderValue();
   expect(webCryptSession.userId).toBeUndefined();
   expect(nonSessionHeader).toBeUndefined();
 });
